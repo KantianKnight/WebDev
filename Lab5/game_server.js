23ebffef9b9ff41d3451610a91caef803a8917d1
Darren Chua <darrenchua314@gmail.com>
2026-04-19 00:30:16 +0800

    lab5

---
+++ Lab5/game_server.js
@@ -0,0 +1,149 @@
+const express = require("express");
+
+// Additional packages for handling authentication
+const argon2 = require("argon2");
+const fs = require("fs");
+const session = require("express-session");
+
+// Create the Express app
+const app = express();
+
+// Use the 'public' folder to serve static files
+app.use(express.static("public"));
+
+// Use the json middleware to parse JSON data
+app.use(express.json());
+
+// Use the session middleware to maintain sessions
+const gameSession = session({
+    secret: "game",
+    resave: false,
+    saveUninitialized: false,
+    rolling: true,
+    cookie: { maxAge: 300000 }
+});
+app.use(gameSession);
+
+// This helper function checks whether the text only contains word characters
+function containWordCharsOnly(text) {
+    return /^\w+$/.test(text);
+}
+
+// Handle the /register endpoint
+app.post("/register", async (req, res) => {
+    //
+    // C. Reading the json input
+    //
+
+    // Get the JSON data from the body
+    const { username, avatar, name, password } = req.body;
+
+    //
+    // D. Reading the users.json file
+    //
+
+    // Add your code here
+    
+    //
+    // E. Checking for the user data correctness
+    //
+    
+    // Add your code here
+
+    //
+    // G. Adding the new user account
+    //
+
+    // Hash the password
+    const hash = await argon2.hash(password);
+
+    // Add your code here
+
+    //
+    // H. Saving the users.json file
+    //
+
+    // Add your code here
+
+    //
+    // I. Sending a success response to the browser
+    //
+ 
+    res.json({ error: "Not implemented!" });
+});
+
+// Handle the /signin endpoint
+app.post("/signin", async (req, res) => {
+    // Get the JSON data from the body
+    const { username, password } = req.body;
+
+    //
+    // D. Reading the users.json file
+    //
+
+    // Add your code here
+
+    //
+    // E. Checking for username/password
+    //
+
+    // Add your code here
+
+    // REPLACE THIS CODE WITH YOUR CODE
+    const user = { password: await argon2.hash("") };
+
+    // If password is incorrect, return an error
+    const verified = await argon2.verify(user.password, password);
+    if (!verified) {
+        res.json({ error: "Incorrect username/password." });
+        return;
+    }
+
+    //
+    // G. Sending a success response with the user account
+    //
+
+    // Add your code here
+ 
+    res.json({ error: "Not implemented!" });
+});
+
+// Handle the /validate endpoint
+app.get("/validate", (req, res) => {
+
+    //
+    // B. Getting req.session.user
+    //
+
+    // Add your code here
+
+    //
+    // D. Sending a success response with the user account
+    //
+
+    res.json({ error: "Not implemented!" });
+});
+
+// Handle the /signout endpoint
+app.get("/signout", (req, res) => {
+
+    //
+    // Deleting req.session.user
+    //
+
+    // Delete the session information
+    if (req.session.user) {
+        delete req.session.user;
+    }
+
+    //
+    // Sending a success response
+    //
+
+    res.json({ success: true });
+});
+
+// Use a web server to listen at port 8000
+app.listen(8000, () => {
+    console.log("The game server has started...");
+});
